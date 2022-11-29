26f3bea21d572125ea286d234770fa47efb91e6d
John Freeman <jcfree@mu2edaq13.fnal.gov>
2022-11-29 12:37:08 -0600

    JCF: the exact code found in ATLAS TDAQ's "master" commit of its "oks_utils" repo, used in the ATLAS tdaq-09-05-00 release

---
+++ oks/access.h
@@ -0,0 +1,25 @@
+#ifndef OKS_ACCESS_H
+#define OKS_ACCESS_H
+
+#include <oks/kernel.h>
+
+namespace oks
+{
+  namespace access
+  {
+    /**
+     * The function tests write access to database file.
+     *
+     * If the file is stored on OKS server repository, it checks access
+     * permissions using Access Manager service. In this case optional user
+     * name can be provided instead of current user taken by default.
+     *
+     * Otherwise it checks file system directory permissions for current user.
+     */
+
+    bool
+    is_writable(const OksFile& file, const std::string& user = OksKernel::get_user_name());
+  }
+}
+
+#endif
---
+++ oks/tz.h
@@ -0,0 +1,68 @@
+#ifndef OKS_TZ_H
+#define OKS_TZ_H
+
+#include <string>
+#include <vector>
+
+#include <boost/date_time/local_time/local_time.hpp>
+#include <boost/date_time/posix_time/ptime.hpp>
+
+namespace oks
+{
+  namespace tz
+  {
+
+    /**
+     * The class provides access to Boost time-zone database.
+     * It relies on Boost time-zone database file installed by oks_utils package
+     * and pointed by BOOST_DATE_TIME_TZ_SPEC process environment variable.
+     */
+
+    class DB
+    {
+    public:
+
+      /**
+       * Find and read Boost time-zone database.
+       */
+      DB();
+
+      /**
+       *  \brief Get boost time-zone pointer from string.
+       *
+       *  This is a helper method for several utilities dealing with Boost time-zone database.
+       *
+       *  \param    region name of time zone
+       *  \return   time-zone boost pointer
+       *  \throw    std::exception if something goes wrong
+       */
+      boost::local_time::time_zone_ptr
+      get_tz_ptr(const std::string& region);
+
+      /**
+       *  \brief Get known time zone regions.
+       *
+       *  This is a helper function for several utilities dealing with Boost time-zone database.
+       *
+       *  \return   vector of known regions
+       *  \throw    std::exception if something goes wrong
+       */
+      std::vector<std::string>
+      get_regions();
+
+    private:
+      boost::local_time::tz_database m_tz_db;
+    };
+
+    boost::posix_time::ptime
+    str_2_posix_time(const std::string& in, boost::local_time::time_zone_ptr tz_ptr, const char * value);
+
+    uint64_t
+    posix_time_2_to_ns(boost::posix_time::ptime t);
+
+    std::string
+    posix_time_2_iso_string(boost::posix_time::ptime t);
+  }
+}
+
+#endif
